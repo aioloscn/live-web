ca009a60dacea36a52d1c6842940efa718fa8534
linhao <hao.lin2@zhenai.com>
2023-07-27 10:54:10 +0800

    连接im服务器

--- js/living_room.js
+++ js/living_room.js
@@ -15,8 +15,9 @@ new Vue({
         userId: -1,
         isLogin: false,
         wsServer: '',
-        imToken: '',
+        imToken: 'qwenaskjn=-1238yu1298hdasjkbxckjwe',
         initInfo: {},
+        imServerConfig: {},
         showGiftRank: false,
         rankList: [],
         accountInfo: {},
@@ -31,7 +32,8 @@ new Vue({
     },
 
     mounted() {
-        this.anchorConfigUrl();
+        this.connectImServer();
+        // this.anchorConfigUrl();
     },
 
     beforeDestroy() {
@@ -40,6 +42,54 @@ new Vue({
 
     methods: {
 
+        connectImServer: function() {
+            let that = this;
+            httpPost(getImConfigUrl, {})
+            .then(resp => {
+                if (isSuccess(resp)) {
+                    that.imServerConfig = resp.data;
+                    let url = "ws://"+that.imServerConfig.wsImServerAddress+"/token=" + that.imServerConfig.token+"&&userId=167394";
+                    console.log(url);
+                    that.websock = new WebSocket(url);
+                    that.websock.onmessage = that.websocketOnMessage;
+                    that.websock.onopen = that.websocketOnOpen;
+                    that.websock.onerror = that.websocketOnError;
+                    that.websock.onclose = that.websocketClose;
+                    console.log('初始化ws服务器');
+                }
+            });
+           
+        },
+
+        websocketOnOpen() {
+            console.log('初始化连接建立');
+            //  //发送一个登录的心跳包给到服务端
+            //  var jsonStr = {"userId": 100111, "appId": 10001, "token": this.imToken};
+            //  let bodyStr = JSON.stringify(jsonStr);
+            //  if(bodyStr!=null) {
+            //     let loginPkg = {'magic':19231,'code':1001,'len': bodyStr.length, 'body': bodyStr};
+            //     this.websocketSend(JSON.stringify(loginPkg));
+            //  }
+        },
+
+        websocketOnError() {
+            console.error('出现异常');
+        },
+
+        websocketOnMessage(e) { //数据接收
+            console.log(e);
+        },
+
+        websocketSend(data) {//数据发送
+            console.log(data);
+            this.websock.send(data);
+        },
+
+        websocketClose(e) {  //关闭
+            console.log('断开连接', e);
+        },
+
+        //直播间初始化配置加载时候调用
         anchorConfigUrl: function () {
             let data = new FormData();
 			data.append("roomId",getQueryStr("roomId"));
@@ -75,12 +125,6 @@ new Vue({
                 });
                 return;
             }
-            let msgBodyStr = {"senderId": this.userId, "livingRoomId": this.roomId, "source": 1, "content": this.form.review};
-            let msgBody = {"code": 1, "body": JSON.stringify(msgBodyStr)};
-            let sendMsgPkg = {"bizCode": 0, "type": 1, "token": this.imToken, "userId": this.userId, "content": msgBody};
-            let sendMsgStr = JSON.stringify(sendMsgPkg);
-            let sendMsgPkgInfo = {'contentLength': sendMsgStr.length, 'body': sendMsgStr};
-
             let sendMsg = {"content": this.form.review, "senderName": this.initInfo.nickname, "senderImg": this.initInfo.avatar};
             let msgWrapper = {"msgType": 1, "msg": sendMsg};
             this.chatList.push(msgWrapper);
